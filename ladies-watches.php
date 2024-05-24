ee84d9981f79b2b200c77dc5789244d576aeb5b6
Caroline <wanguic49@gmail.com>
2024-05-24 10:04:54 +0300

    few changes made

---
+++ ladies-watches.php
@@ -0,0 +1,21 @@
+<?php
+    require('config.php');
+
+    $sql = "SELECT * FROM glitterdetails
+    WHERE genderclass = 'ladies' AND  category = 'watches'";
+    $results = $conn->query($sql);
+
+    if ($results->num_rows > 0) {
+        $data = array();
+
+        while ($row = $results->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        echo json_encode($data);
+    } else {
+        echo json_encode(['message' => 'no results found!']);
+    }
+
+    $conn->close();
+?>
---
+++ mens-watches.php
@@ -0,0 +1,22 @@
+<?php
+    require('config.php');
+
+    $sql = "SELECT * FROM glitterdetails
+    WHERE genderclass = 'men' AND  category = 'watches'";
+    
+    $results = $conn->query($sql);
+
+    if ($results->num_rows > 0) {
+        $data = array();
+
+        while ($row = $results->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        echo json_encode($data);
+    } else {
+        echo json_encode(['message' => 'no results found!']);
+    }
+
+    $conn->close();
+?>
